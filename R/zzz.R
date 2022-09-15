a6b031537bef1c6cd917b871edc85afd862b97a9
Lionir <eygen.chekalin@gmail.com>
2022-09-15 10:08:10 -0500

    0.38 4nd review

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,6 @@
 #' @export
 #' @importFrom ExperimentHub ExperimentHub
 
-.my_internal_global_variables <- new.env(parent=emptyenv())
-
-.get_eh <- function() get("eh", envir=.my_internal_global_variables)
-.set_eh <- function(value) assign("eh", value,
-                                  envir=.my_internal_global_variables)
 
 get_ExperimentHub_data  <- function(file=NULL)
 {
@@ -20,6 +15,15 @@ get_ExperimentHub_data  <- function(file=NULL)
     return(eh[[file]])
   }
 }
+
+
+.my_internal_global_variables <- new.env(parent=emptyenv())
+
+.get_eh <- function() get("eh", envir=.my_internal_global_variables)
+.set_eh <- function(value) assign("eh", value,
+                                  envir=.my_internal_global_variables)
+
+
 #.eh <- NULL
 
 #.onLoad <- function(libname, pkgname)
