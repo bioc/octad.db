076774c63fbb9e2221e6d3e080f864120a710758
Lionir <eygen.chekalin@gmail.com>
2022-10-16 14:45:58 -0500

    0.43 pre-release

--- R/zzz.R
+++ R/zzz.R
@@ -2,21 +2,6 @@
 #' @importFrom ExperimentHub ExperimentHub
 
 
-get_ExperimentHub_data  <- function(file=NULL)
-{
-  eh <- try(.get_eh(), silent=TRUE)
-  if (inherits(eh, "try-error")) {
-    eh <- ExperimentHub()
-    .set_eh(eh)
-  }
-  if(is.null(file)){
-    return(eh)
-  }else{
-    return(eh[[file]])
-  }
-}
-
-
 .my_internal_global_variables <- new.env(parent=emptyenv())
 
 .get_eh <- function() get("eh", envir=.my_internal_global_variables)
